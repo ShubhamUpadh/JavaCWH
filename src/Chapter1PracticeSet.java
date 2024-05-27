80b72ea5083a15b105633ae893d92d2ad1eb9367
ShubhamUpad <01shubhamupadhyay@gmail.com>
2024-05-27 22:13:44 +0530

    Questions finished

--- src/Chapter1PracticeSet.java
+++ src/Chapter1PracticeSet.java
@@ -1,16 +1,24 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Chapter1PracticeSet {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter marks in subject 1 : ");
-        int a = sc.nextInt();
-        System.out.println("Enter marks in subject 2 : ");
-        int b = sc.nextInt();
-        System.out.println("Enter marks in subject 3 : ");
-        int c = sc.nextInt();
-
-        System.out.println("The CGPA is " + (float)(a+b+c)/30);
+//        System.out.println("Enter marks in subject 1 : ");
+//        int a = sc.nextInt();
+//        System.out.println("Enter marks in subject 2 : ");
+//        int b = sc.nextInt();
+//        System.out.println("Enter marks in subject 3 : ");
+//        int c = sc.nextInt();
+//
+//        System.out.println("The CGPA is " + (float)(a+b+c)/30);
+//        System.out.println("What is your name ? ");
+//        String nm = sc.nextLine();
+//
+//        System.out.println("Hello " + nm +", have a good day ");
+        System.out.println("I will tell if the number is an integer or not \nEnter an input ");
+        boolean a  = sc.hasNextInt();
+        System.out.println(a);
     }
 }
