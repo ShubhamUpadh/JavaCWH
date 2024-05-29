2905bb26b28987eeb407adb3d46eef77d1ae21a0
ShubhamUpad <01shubhamupadhyay@gmail.com>
2024-05-29 23:11:01 +0530

    lecture 15 done

--- src/Chapter3PracticeSet.java
+++ src/Chapter3PracticeSet.java
@@ -1,2 +1,11 @@
 public class Chapter3PracticeSet {
+    public static void main(String[] args) {
+
+        String st = "Ehhele lele  e erere";
+        System.out.println(st.replace(" ","_"));
+
+        String letter = " \"Dear Harry, this java course is good";
+        System.out.println(letter);
+    }
+
 }
