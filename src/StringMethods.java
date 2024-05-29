e6c7a0a6aa5b13f30f0bdd292fe9ff6d231fe59e
ShubhamUpad <01shubhamupadhyay@gmail.com>
2024-05-29 21:58:51 +0530

    lecture 4 done

---
+++ src/StringMethods.java
@@ -0,0 +1,29 @@
+public class StringMethods {
+    public static void main(String[] args) {
+        String s = "stRing";
+        /*
+        s = s + "1";
+        System.out.println(s);
+        int len = s.length();
+        System.out.println(len);
+        String lString = s.toLowerCase();
+        System.out.println(lString);
+        String uString = s.toUpperCase();
+
+        String ntc = "   My name is  Shubham   ";
+        System.out.println(ntc);
+        String tc = ntc.trim();
+        System.out.println(tc);
+
+        System.out.println(s.substring(4));
+
+        String a = "Shubham";
+        System.out.println(a.replace("ub","vf"));
+        */
+
+        String st = "Hellollollo";
+        System.out.println(st.indexOf("llo",6));
+
+        System.out.println(true);
+    }
+}
