a62f45d430a4b6b2fd8094ef48365ebb5db8443d
ShubhamUpad <01shubhamupadhyay@gmail.com>
2024-05-30 23:35:29 +0530

    Arrays lec 27 done

--- src/arrays.java
+++ src/arrays.java
@@ -1,2 +1,16 @@
 public class arrays {
+    public static void main(String[] args) {
+        int [] marks = new int [5];
+        for (int j =0;j<marks.length;j++){
+            marks[j] = 100 - 27*j + j*j;
+        }
+        for (int mark : marks) {
+            System.out.println(mark);
+        }
+
+        int [] ide = {1,2,3};
+        for (int i : ide){
+            System.out.println(i);
+        }
+    }
 }
