1fc82abea6181f06e9a5d1969ca6f75372c6eec9
ShubhamUpad <01shubhamupadhyay@gmail.com>
2024-06-24 19:42:31 +0530

    Det Zeroes

---
+++ src/SetZeroes.java
@@ -0,0 +1,18 @@
+public class SetZeroes {
+    static setZeroes(int[][] matrix){
+        int[] row = new int[matrix.length];
+        int[] column = new int[matrix[0].length];
+
+        for (int i = 0; i < matrix.length; i++){
+            for (int j = 0; j < matrix[0].length; j++){
+                if (matrix[i][j] == 0){
+                    row[i] = -1;
+                    column[j] = -1;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+
+    }
+}
