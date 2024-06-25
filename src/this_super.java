8e5e630f4d50d9f65f1ba156e8c193ec0edb424d
ShubhamUpad <01shubhamupadhyay@gmail.com>
2024-06-26 01:43:31 +0530

    super() this done

--- src/this_super.java
+++ src/this_super.java
@@ -1,2 +1,22 @@
+class EkClass{
+    int a;
+
+    public int getA() {
+        return a;
+    }
+    public EkClass(int a) {
+        this.a = a;
+    }
+
+
+}
+
+class DoClass extends EkClass{
+    DoClass(int x){
+        super(x);
+    }
+}
+
+
 public class this_super {
 }
