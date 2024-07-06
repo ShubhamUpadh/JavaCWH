98073578166010739d3bf9b98e929c20903400cd
ShubhamUpad <01shubhamupadhyay@gmail.com>
2024-07-06 22:11:30 +0530

    Interface done

---
+++ src/interfaces.java
@@ -0,0 +1,21 @@
+interface cycle{
+    void applybrake(int x);
+    void speedup(int y);
+}
+
+class HeroCycle implements cycle{
+    public HeroCycle(int speed){
+        this.speed = speed;
+    }
+    @Override
+    public void applybrake(int decrement){
+        this.speed -= decrement;
+    }
+    @Override
+    public void speedup(int increment){
+        this.speed += increment;
+    }
+}
+
+public class interfaces {
+}
