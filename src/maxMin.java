d2ff98fbc0324abb1bc76fec7f9841f92f2b9b39
ShubhamUpad <01shubhamupadhyay@gmail.com>
2024-07-14 22:11:42 +0530

    Done minMax

---
+++ src/maxMin.java
@@ -0,0 +1,12 @@
+public class maxMin {
+    public static void main(String[] args) {
+
+    }
+    static int solution(int[] arr){
+        int min = arr[0], max = arr[0];
+        for (int i = 1; i < arr.length; i++){
+            min = Math.min(min. arr[i]);
+            max = Math.max(max, arr[i]);
+        }
+    }
+}
