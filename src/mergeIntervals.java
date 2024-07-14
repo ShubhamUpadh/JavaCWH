957b329d5a24d850b2e6cfa258cb7077094c5c94
ShubhamUpad <01shubhamupadhyay@gmail.com>
2024-07-14 23:03:04 +0530

    Done mergeIntervals

---
+++ src/mergeIntervals.java
@@ -0,0 +1,36 @@
+public class mergeIntervals {
+    public static void main(String[] args) {
+
+    }
+    static int[][] solution(int[][] arr, int[] interval){
+        int[] numberLine = new int[arr[arr.length-1][1] + 1];
+        for (int[] intArr : arr) {
+            numberLine[intArr[0]] = 1;
+            numberLine[intArr[1]] = -1;
+        }
+        boolean intervalStart = false, mergeIntervalStart = false;
+        int i = 0;
+        while (i < numberLine.length){
+            if (numberLine[i] == 1)
+                intervalStart = true;
+
+            else if (numberLine[i] == -1)
+                intervalStart = false;
+
+            if (i == interval[0]){
+                if (!intervalStart) numberLine[i] = 1;
+                mergeIntervalStart = true;
+                while (i < numberLine.length && i < interval[1]){
+                    if (numberLine[i] != 0){
+                        numberLine[i] = 0;
+                    }
+                    i++;
+                }
+                if (numberLine[i] == 1) numberLine[i] = 0;
+                else numberLine[i] = -1;
+            }
+            i++;
+        }
+        return new int[]{1,1};
+    }
+}
