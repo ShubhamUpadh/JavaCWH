8001b889b552d95111991fb44f24e3ceccb674eb
ShubhamUpad <01shubhamupadhyay@gmail.com>
2024-07-30 03:12:16 +0530

    Added makeElementsEqual.java

--- src/makeElementsEqual.java
+++ src/makeElementsEqual.java
@@ -1,7 +1,21 @@
 public class makeElementsEqual {
-    static int solution(int[] A){
-        for (int a : A){
-            if (a == A[0] || a == A[0]-B || a == A[0]+B){
+    public static void main(String[] args) {
+        int[] A =  { 3, 1, 2, 1, 3, 1, 2, 2, 1, 1, 2, 3, 3, 1, 3, 3, 3, 2, 3, 3, 1, 1, 1, 3, 3, 1, 3, 1, 3, 3, 1, 1, 3, 2, 2, 3, 1, 2, 1, 3, 3, 3, 2, 1, 1, 2, 1 };
+        System.out.println(solution(A, 1));
+
+    }
+
+    static boolean isPresent(int val,int[] values){
+        if (val == values[0] || val == values[1] || val == values[2]){
+            return true;
+        }
+        return false;
+    }
+    static int solution(int[] A,int B){
+        int[] values = {A[0], A[0]-B, A[0]+B};
+
+        for (int i = 1; i < A.length; i++){
+            if (isPresent(A[i],values) || isPresent(A[i] + B, values) || isPresent(A[i]-B, values)){
                 continue;
             }
             else return 0;
