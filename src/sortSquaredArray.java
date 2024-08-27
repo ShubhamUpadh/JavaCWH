455bb8b6e399da806946d08ee7bf972503dec0fc
ShubhamUpad <01shubhamupadhyay@gmail.com>
2024-08-27 23:54:50 +0530

    Added Changes

--- src/sortSquaredArray.java
+++ src/sortSquaredArray.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class sortSquaredArray {
     public static void main(String[] args) {
-        int[] arr = {-6, -3, -1, 2, 4, 5};
+        int[] arr = {51, -855, -847, -747, -708, -701, -667, -666, -618, -609, -536, -533, -509, -500, -396, -336, -297, -284, -229, -173, -173, -132, -38, -5, 35, 141, 169, 281, 322, 358, 421, 436, 447, 478, 538, 547, 644, 667, 673, 705, 711, 718, 724, 726, 811, 869, 894, 895, 902, 912, 942, 961};
         System.out.println(Arrays.toString(sol(arr)));
     }
     static int[] sol(int[] arr){
