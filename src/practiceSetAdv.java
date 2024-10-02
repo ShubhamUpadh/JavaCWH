7235bdfd4b75bc922515e7882a272ba70655532d
ShubhamUpad <01shubhamupadhyay@gmail.com>
2024-10-02 12:46:13 +0530

    Added practiceSet

--- src/practiceSetAdv.java
+++ src/practiceSetAdv.java
@@ -2,7 +2,9 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class practiceSetAdv {
     static ArrayList<String > ques1(){
@@ -27,10 +29,26 @@ public class practiceSetAdv {
         return d.format(df);
     }
 
+    static void duplicateSets(){
+        HashSet<Integer> s = new HashSet<>();
+        ArrayList<Integer> al = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
 
+        for (int i = 0; i < 10; i++){
+            System.out.println("Enter an integer value");
+            int val = sc.nextInt();
+            s.add(val);
+            al.add(val);
+        }
+
+        System.out.println(s);
+        System.out.println(al);
+
+    }
 
     public static void main(String[] args) {
         //ques1();
-        System.out.println(timeFormat());
+        //System.out.println(timeFormat());
+        duplicateSets();
     }
 }
