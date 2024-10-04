c980dcf6db3c57f9d3f01f61166f56a53c288037
ShubhamUpad <01shubhamupadhyay@gmail.com>
2024-10-04 19:31:06 +0530

    Added smallerOrEqualElement

--- src/smallerOrEqualElements.java
+++ src/smallerOrEqualElements.java
@@ -1,6 +1,6 @@
 public class smallerOrEqualElements {
-    static  int sol(int[] arr){
-        int b = 4;
+    static  int sol(int[] arr, int b){
+        //int b = 4;
         int low = 0, high = arr.length - 1;
 
         if (arr[low] > b) return 0;
@@ -9,12 +9,19 @@ public class smallerOrEqualElements {
         while (low <= high) {
             mid = (low + high) / 2;
             System.out.println(low + " " + mid + " " + high);
-            if (arr[mid] == b && arr[mid+1] > b){
+            if ((arr[mid] == b && arr[mid+1] > b) || (arr[mid] < b && arr[mid + 1] > b)){
+                System.out.println("Case 1");
                 return mid + 1;
             }
+            else if (arr[mid] > b && arr[mid - 1] < b){
+                System.out.println("Case 2");
+                return mid;
+            }
             else if ((arr[mid] < b) || (arr[mid] == b && arr[mid + 1] == b)) {
+                System.out.println("Case 3");
                 low = mid + 1;
             } else {
+                System.out.println("Case 4");
                 high = mid - 1;
             }
         }
@@ -25,7 +32,7 @@ public class smallerOrEqualElements {
 
 
     public static void main(String[] args) {
-        int[] arr = {1,3,4,4,6};
-        System.out.println(sol(arr));
+        int[] arr = {1, 3, 3, 3, 6, 6, 6, 6, 6, 7};
+        System.out.println(sol(arr,2));
     }
 }
