955603238e5fda461554f8fcfa85e57c0c526755
ShubhamUpad <01shubhamupadhyay@gmail.com>
2024-10-16 00:52:17 +0530

    Median of two sorted arrays

---
+++ src/classVariable.java
@@ -0,0 +1,17 @@
+class newClass{
+    public static int a = 10;
+    public int method1(){
+        System.out.println("Initialised");
+    }
+}
+public class classVariable {
+    public static void main(String[] args) {
+        newClass obj1 = new newClass();
+        newClass.a = 100;
+        newClass obj2 = new newClass();
+
+        newClass obj3 = new newClass();
+
+
+    }
+}
---
+++ src/medianOfArray.java
@@ -0,0 +1,9 @@
+public class medianOfArray {
+    public static void main(String[] args) {
+
+    }
+    static int median(int[] A, int[] B){
+        int[] arr = new int[A.length + B.length];
+
+    }
+}
