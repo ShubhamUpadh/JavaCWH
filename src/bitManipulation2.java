58f4b748f72f60424a7456c394ef841fc79066bd
ShubhamUpad <01shubhamupadhyay@gmail.com>
2024-10-22 23:43:23 +0530

    added bit manip 2

--- src/bitManipulation2.java
+++ src/bitManipulation2.java
@@ -3,7 +3,9 @@ import java.util.Arrays;
 public class bitManipulation2 {
     public static void main(String[] args) {
         //System.out.println(Arrays.toString(swap(10, 15)));
-        System.out.println(isBitSet(8,3));
+        //System.out.println(isBitSet(8,3));
+        //System.out.println(setIthBit(9, 2));
+        System.out.println(clearIthBit(13,2));
     }
     static int[] swap(int a, int b){
         a = a ^ b;
@@ -15,5 +17,15 @@ public class bitManipulation2 {
     static boolean isBitSet(int A, int i){
         //A = A >> i;
         return (A>>i) % 2 != 0;
+    }
+
+    static int setIthBit(int A, int i){
+        return A | (1 << i);
+    }
+
+    static int clearIthBit(int A, int i){
+        {
+            return (A & ~(1<<i));
+        }
     }
 }
