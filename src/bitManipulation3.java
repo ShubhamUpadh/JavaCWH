9dd5b5e949c0c18036bb349028e34cf3a78010dd
ShubhamUpad <01shubhamupadhyay@gmail.com>
2024-11-06 00:00:58 +0530

    added notes

--- src/bitManipulation3.java
+++ src/bitManipulation3.java
@@ -1,12 +1,13 @@
 public class bitManipulation3 {
     public static void main(String[] args) {
-        System.out.println(minFlips(10,7));
+        System.out.println(minFlips(4,7));
     }
     static int minFlips(int A, int B){
-        int temp = A ^ B;
+        int temp = A ^ B;       // 7 ^ 4 = 111 ^ 100 = 011
         int counter = 0;
         while (temp > 0){
-            temp = temp & (temp -1);
+            temp = temp & (temp -1); // temp = (011)&(010) = (010), (010)^(001) = 0
+            System.out.println(temp);
             counter++;
         }
         return counter;
---
+++ src/bitManipulation9SetBitCountInRange.java
@@ -0,0 +1,22 @@
+public class bitManipulation9SetBitCountInRange {
+    public static void main(String[] args) {
+
+    }
+    static int sol(int A){
+        int pow = 0;
+        while (true){
+            if (1<<pow > A) break;
+            pow++;
+        }
+        pow--;
+
+        int ans = 0;
+
+        for(int i = 0; i < pow; i++){
+
+        }
+
+        return 10;
+
+    }
+}
