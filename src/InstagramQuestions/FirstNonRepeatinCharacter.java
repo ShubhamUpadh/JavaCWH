25f80e6c5b5a06e442f51fd9606ffa282fac81bb
ShubhamUpad <01shubhamupadhyay@gmail.com>
2024-12-06 00:10:55 +0530

    added FirstNonRepeatingCharacter.java

---
+++ src/InstagramQuestions/FirstNonRepeatinCharacter.java
@@ -0,0 +1,22 @@
+package InstagramQuestions;
+
+public class FirstNonRepeatinCharacter {
+    public static void main(String[] args) {
+        String st = "swiss";
+        System.out.println(sol(st));
+    }
+    static char sol(String st){
+        int[] arr = new int[26];
+
+        for (char a : st.toCharArray())  arr[a - 'a']++;
+
+        for (char a : st.toCharArray()){
+            if (arr[a - 'a'] == 1){
+                return a;
+            }
+        }
+        return '1';
+
+
+    }
+}
