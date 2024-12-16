c145b221c0c9cde0621aa7abcd4874d123f0b7ef
ShubhamUpad <01shubhamupadhyay@gmail.com>
2024-12-16 22:42:25 +0530

    added BSTRightView

--- src/BinaryTree/Tree09LeftView.java
+++ src/BinaryTree/Tree09LeftView.java
@@ -10,10 +10,11 @@ public class Tree09LeftView {
     public static void main(String[] args) {
         TreeUtility treeUtility = new TreeUtility();
         Node root = treeUtility.inputToTree();
-        sol(root);
+        leftView(root);
+        rightView(root);
     }
 
-    static void sol(Node root){
+    static void leftView(Node root){
         TreeMap<Integer, Integer> map = new TreeMap<>();
 
         Queue<Pair> q = new LinkedList<>();
@@ -32,6 +33,33 @@ public class Tree09LeftView {
                 if (!map.containsKey(currDistance)){
                     map.put(currDistance,currNode.value);
                 }
+
+                if (currNode.left != null) q.add(new Pair(currNode.left, currDistance + 1));
+                if (currNode.right != null) q.add(new Pair(currNode.right, currDistance + 1));
+            }
+
+        }
+        for (Map.Entry<Integer, Integer> e : map.entrySet()){
+            System.out.println(e.getValue());
+        }
+    }
+    static void rightView(Node root){
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+
+        Queue<Pair> q = new LinkedList<>();
+
+        q.add(new Pair(root, 0));
+
+        while (!q.isEmpty()){
+            int size = q.size();
+            for (int i = 0; i < size; i++){
+                Pair currPair = q.poll();
+                Node currNode = currPair.node;
+                int currDistance = currPair.distance;
+
+                //System.out.println(currNode.value);
+
+                    map.put(currDistance,currNode.value);
 
                 if (currNode.left != null) q.add(new Pair(currNode.left, currDistance + 1));
                 if (currNode.right != null) q.add(new Pair(currNode.right, currDistance + 1));
