d42fea049516356e322fa84fa6504a318b128537
ShubhamUpad <01shubhamupadhyay@gmail.com>
2024-12-27 20:49:29 +0530

    added 1DArrayWaysOfSitting

---
+++ src/Recursion/Recursion16.java
@@ -0,0 +1,41 @@
+package Recursion;
+
+import java.util.Arrays;
+
+public class Recursion16 {
+    public static void main(String[] args) {
+        int pawns = 25;
+        int power = 1;
+        int size = 49;
+
+        int[] arr = new int[size];
+
+        System.out.println(numberOfArrangements(arr, 0, pawns, power, 1));
+    }
+
+    static int numberOfArrangements(int[] arr, int index, int pawns, int power, int pawnIndex){
+        
+        if ((arr.length - index - 1) < (pawns - pawnIndex)*(power + 1)) return 0;
+
+        if (pawnIndex > pawns){
+            System.out.println("Success" + Arrays.toString(arr) + " | " + pawnIndex + " | " + index);
+            return 1;
+        }
+        else{
+            System.out.print("");
+            //System.out.println(Arrays.toString(arr) + " | " + pawnIndex + " | " + index);
+        }
+
+        int ways = 0;
+
+        for (int i = index; i < arr.length; i++){
+            
+            arr[i] = 1;
+            ways += numberOfArrangements(arr, i + power + 1, pawns, power, pawnIndex + 1);
+            arr[i] = 0;
+        }
+
+        return ways;
+    }
+
+}
